12d23010928ff7202fdba51c7224499bfc9168b6
Chans Corbett <chanscorbett@Alveyworld-27.local>
2015-09-03 10:11:40 -0600

    hello

---
+++ bandnames.py
@@ -0,0 +1,20 @@
+# Band name generator
+import random	
+
+
+titles = ["gigantic", "sour", "steamy", "gross", "stupid", 
+		  "ironic", "greasy", "tangy", "smelly", "small", 
+		  "inventive", "spherical", "spiritual", "green",
+		  "blue", "pot bellied", "pickled", "prickly"]
+		  
+adjs = ["tiny", "fat", "shiny", "ecclectic", "fluffy", "bright",
+		"corrupt", "aggressive", "alarming", "amazing", "magical",
+		"courageous", "fierce", "colorless", "red", "thoughtless",
+		"smart", "delirious", "fabulous", "fergalicious", "dangerous"]
+		
+plural_nouns = ["apples", "oranges", "kiwis", "clementines",
+				"wildabeasts", "mammoths", "rabbits", "sloths", "crashes",
+				"spices", "eggs", "herbs", "pony tails", "bears", "snitches", 
+				"unicorns", "kermits", "signs", "indians", "cowboys", "cheetah"]
+				
+ 
